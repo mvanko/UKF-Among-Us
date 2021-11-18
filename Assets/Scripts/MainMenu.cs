4a433bf4fe5fd23329e9d54ffaa4ef5959a4cdc8
whiskeY <andy.pipo7@gmail.com>
2021-11-18 13:29:45 +0100

    Sample main menu scene, added first version of menu script, imported TMP

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _uiMainMenu;
+    [SerializeField] private GameObject _uiSettings;
+
+    [SerializeField] private Button _uiStartButton;
+    [SerializeField] private Button _uiSettingsButton;
+    [SerializeField] private Button _uiQuitButton;
+
+    private const string LEVEL1NAME = "Level1";
+
+    private void Start()
     {
-        
+        OpenMainMenu();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        _uiStartButton.onClick.AddListener(StartGame);
+        _uiSettingsButton.onClick.AddListener(OpenSettings);
+        _uiQuitButton.onClick.AddListener(QuitGame);
+    }
+
+    private void OnDisable()
+    {
+        _uiStartButton.onClick.RemoveListener(StartGame);
+        _uiSettingsButton.onClick.RemoveListener(OpenSettings);
+        _uiQuitButton.onClick.RemoveListener(QuitGame);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(LEVEL1NAME);
+    }
+
+    private void OpenSettings()
+    {
+        _uiMainMenu.SetActive(false);
+        _uiSettings.SetActive(true);
+    }
+
+    private void OpenMainMenu()
+    {
+        _uiMainMenu.SetActive(true);
+        _uiSettings.SetActive(false);
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
     }
 }
