5f422fcdd927e8598b8807ad6049abbb07107c1d
mvanko <48046136+mvanko@users.noreply.github.com>
2021-12-12 21:02:17 +0100

    Added voting buttons
    Voting buttons now spawn according to number of players in game and changed the ui of chat and buttons

--- Assets/Scripts/Game/ReportHUD.cs
+++ Assets/Scripts/Game/ReportHUD.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Chat;
+using Photon.Realtime;
+using System.IO;
 
 public class ReportHUD : MonoBehaviour
 {
@@ -11,6 +16,11 @@ public class ReportHUD : MonoBehaviour
 
     private bool chatVisible = false;
     private float votingTime = 101f;
+    private Scene scene;
+
+    [SerializeField] private GameObject[] buttonSpawns;
+
+    private GameObject _playerButton;
 
     private void Awake()
     {
@@ -20,7 +30,13 @@ public class ReportHUD : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        scene = SceneManager.GetActiveScene();
+        foreach (Photon.Realtime.Player p in PhotonNetwork.PlayerList)
+        {
+            buttonSpawns[p.ActorNumber - 1].SetActive(true);
+            buttonSpawns[p.ActorNumber - 1].GetComponentInChildren<Text>().text = p.NickName;
+
+        }
     }
 
     // Update is called once per frame
@@ -28,6 +44,12 @@ public class ReportHUD : MonoBehaviour
     {
         votingTime -= Time.deltaTime;
         votingTimeText.text = ((int)votingTime).ToString();
+
+        if(votingTime <= 0)
+        {
+            votingTime = 0;
+            SceneManager.LoadScene(scene.name);
+        }
     }
 
     private void OnDestroy()
