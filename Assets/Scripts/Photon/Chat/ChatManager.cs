e1471c0544d16ef8c680e861f42faf6935a48f07
mvanko <48046136+mvanko@users.noreply.github.com>
2021-12-13 14:07:04 +0100

    Map changes, chat update
    Moved light minigame to toilet, moved spawns to "vestibul", added triggers for showing location names, added chat to voting

--- Assets/Scripts/Photon/Chat/ChatManager.cs
+++ Assets/Scripts/Photon/Chat/ChatManager.cs
@@ -1,14 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Chat;
-using ExitGames.Client.Photon;
 using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+using UnityEngine.UIElements;
+using System;
+using System.Collections.Generic;
 
-public class ChatManager : MonoBehaviour, IChatClientListener
+public class ChatManager : MonoBehaviour, IChatClientListener, IOnEventCallback
 {
+    public const byte SendMessageToAllPlayers = 1;
+
+    [SerializeField] Text toSendText;
+    [SerializeField] UnityEngine.UI.Button sendButton;
+    [SerializeField] GameObject messageObject;
+    [SerializeField] Transform contentObject;
+
     ChatClient chatClient;
-    [SerializeField] string userID;
+
+    private string userID;
+
+    private Text textChild;
 
     public void DebugReturn(DebugLevel level, string message)
     {
@@ -68,13 +81,61 @@ public class ChatManager : MonoBehaviour, IChatClientListener
     // Start is called before the first frame update
     void Start()
     {
+        userID = PhotonNetwork.LocalPlayer.NickName;
         chatClient = new ChatClient(this);
-        chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat, PhotonNetwork.AppVersion, new AuthenticationValues(userID));
+        chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat, PhotonNetwork.AppVersion, new Photon.Chat.AuthenticationValues(userID));
+        sendButton.onClick.AddListener(GetChatMessage);
+    }
+
+    private void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    private void OnDestroy()
+    {
+        sendButton.onClick.RemoveListener(GetChatMessage);
     }
 
     // Update is called once per frame
     void Update()
     {
         chatClient.Service();
+    }
+
+    private void GetChatMessage()
+    {
+        string textík = toSendText.text;
+        toSendText.text = "";
+        SendToAll(textík, userID);
+
+    }
+
+    private void SendToAll(string message, string playerName)
+    {
+        string content = playerName+": " + message;
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All };
+        PhotonNetwork.RaiseEvent(SendMessageToAllPlayers, content, raiseEventOptions, SendOptions.SendReliable);
+    }
+
+    public void OnEvent(EventData photonEvent)
+    {
+        if (photonEvent.Code == SendMessageToAllPlayers)
+        {
+            if (contentObject != null)
+            {
+                string messageToShow = (string)photonEvent.CustomData;
+                textChild = messageObject.GetComponentInChildren<Text>();
+                textChild.text = messageToShow;
+                GameObject showMessageObject = Instantiate(messageObject);
+                showMessageObject.transform.SetParent(contentObject);
+                showMessageObject.transform.localScale = new Vector3(1,1,1);
+            }
+        }
     }
 }
